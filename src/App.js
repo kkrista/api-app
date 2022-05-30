56718cadcb2001911832430571d87a7fb949a989
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-05-30 14:28:55 +0300

    Footer with contact information added

--- src/App.js
+++ src/App.js
@@ -33,9 +33,32 @@ function App() {
           </div>
         </div>
       </header>
-      {/* <div className="logo">
-          <img src={logo} max-width="100%" />
-        </div> */}
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/kristamarikatajisto/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Krista-Mari Katajisto
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/kkrista/api-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://upbeat-nobel-41a5b2.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
